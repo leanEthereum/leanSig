9f2b0bbaa9deee3d072122b32c39b8027c3fccd8
Varun Doshi <doshivarun202@gmail.com>
2025-10-21 12:42:59 +0530

    Add instantiation for testing (#91)
    Add SIGTopLevelTargetSumLifetime8Dim64Base8 for testing

--- src/signature/generalized_xmss/instantiations_poseidon_top_level.rs
+++ src/signature/generalized_xmss/instantiations_poseidon_top_level.rs
@@ -323,3 +323,88 @@ pub mod lifetime_2_to_the_32 {
         }
     }
 }
+
+/// Instantiations with Lifetime 2^8. This is for testing purposes only.
+///
+/// Warning: Should not be used in production environments.
+pub mod lifetime_2_to_the_8 {
+    use crate::{
+        inc_encoding::target_sum::TargetSumEncoding,
+        signature::generalized_xmss::GeneralizedXMSSSignatureScheme,
+        symmetric::{
+            message_hash::top_level_poseidon::TopLevelPoseidonMessageHash,
+            prf::shake_to_field::ShakePRFtoF, tweak_hash::poseidon::PoseidonTweakHash,
+        },
+    };
+
+    const LOG_LIFETIME: usize = 8;
+
+    const DIMENSION: usize = 64;
+    const BASE: usize = 8;
+    const FINAL_LAYER: usize = 77;
+    const TARGET_SUM: usize = 375;
+
+    const PARAMETER_LEN: usize = 5;
+    const TWEAK_LEN_FE: usize = 2;
+    const MSG_LEN_FE: usize = 9;
+    const RAND_LEN_FE: usize = 7;
+    const HASH_LEN_FE: usize = 8;
+
+    const CAPACITY: usize = 9;
+
+    const POS_OUTPUT_LEN_PER_INV_FE: usize = 15;
+    const POS_INVOCATIONS: usize = 1;
+    const POS_OUTPUT_LEN_FE: usize = POS_OUTPUT_LEN_PER_INV_FE * POS_INVOCATIONS;
+
+    type MH = TopLevelPoseidonMessageHash<
+        POS_OUTPUT_LEN_PER_INV_FE,
+        POS_INVOCATIONS,
+        POS_OUTPUT_LEN_FE,
+        DIMENSION,
+        BASE,
+        FINAL_LAYER,
+        TWEAK_LEN_FE,
+        MSG_LEN_FE,
+        PARAMETER_LEN,
+        RAND_LEN_FE,
+    >;
+    type TH = PoseidonTweakHash<PARAMETER_LEN, HASH_LEN_FE, TWEAK_LEN_FE, CAPACITY, DIMENSION>;
+
+    type PRF = ShakePRFtoF<HASH_LEN_FE, RAND_LEN_FE>;
+
+    type IE = TargetSumEncoding<MH, TARGET_SUM>;
+
+    pub type SIGTopLevelTargetSumLifetime8Dim64Base8 =
+        GeneralizedXMSSSignatureScheme<PRF, IE, TH, LOG_LIFETIME>;
+
+    #[cfg(test)]
+    mod test {
+
+        use crate::signature::SignatureScheme;
+
+        #[cfg(feature = "slow-tests")]
+        use crate::signature::test_templates::test_signature_scheme_correctness;
+
+        use super::SIGTopLevelTargetSumLifetime8Dim64Base8;
+
+        #[test]
+        pub fn test_internal_consistency() {
+            SIGTopLevelTargetSumLifetime8Dim64Base8::internal_consistency_check();
+        }
+
+        #[cfg(feature = "slow-tests")]
+        #[test]
+        pub fn test_correctness() {
+            test_signature_scheme_correctness::<SIGTopLevelTargetSumLifetime8Dim64Base8>(
+                213,
+                0,
+                SIGTopLevelTargetSumLifetime8Dim64Base8::LIFETIME as usize,
+            );
+            test_signature_scheme_correctness::<SIGTopLevelTargetSumLifetime8Dim64Base8>(
+                4,
+                0,
+                SIGTopLevelTargetSumLifetime8Dim64Base8::LIFETIME as usize,
+            );
+        }
+    }
+}
