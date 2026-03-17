81ab75f719ff62d4b6e4fc9c75326dc3b191cbe7
Tom Wambsgans <TomWambsgans@users.noreply.github.com>
2026-03-17 11:24:00 +0100

    lifetime_2_to_the_32

--- src/signature/generalized_xmss/instantiations_aborting.rs
+++ src/signature/generalized_xmss/instantiations_aborting.rs
@@ -1,8 +1,84 @@
+/// Instantiations with Lifetime 2^32
+pub mod lifetime_2_to_the_32 {
+
+    use crate::{
+        inc_encoding::target_sum::TargetSumEncoding,
+        signature::generalized_xmss::{
+            GeneralizedXMSSPublicKey, GeneralizedXMSSSignature, GeneralizedXMSSSignatureScheme,
+        },
+        symmetric::{
+            message_hash::aborting::AbortingHypercubeMessageHash, prf::shake_to_field::ShakePRFtoF,
+            tweak_hash::poseidon::PoseidonTweakHash,
+        },
+    };
+
+    const LOG_LIFETIME: usize = 32;
+
+    const DIMENSION: usize = 46;
+    const BASE: usize = 8;
+    const TARGET_SUM: usize = 200;
+    const Z: usize = 8;
+    const Q: usize = 127;
+
+    const PARAMETER_LEN: usize = 5;
+    pub const TWEAK_LEN_FE: usize = 2;
+    const MSG_LEN_FE: usize = 9;
+    pub const RAND_LEN_FE: usize = 7;
+    pub const HASH_LEN_FE: usize = 8;
+
+    const CAPACITY: usize = 9;
+
+    type MH = AbortingHypercubeMessageHash<
+        PARAMETER_LEN,
+        RAND_LEN_FE,
+        HASH_LEN_FE,
+        DIMENSION,
+        BASE,
+        Z,
+        Q,
+        TWEAK_LEN_FE,
+        MSG_LEN_FE,
+    >;
+    type TH = PoseidonTweakHash<PARAMETER_LEN, HASH_LEN_FE, TWEAK_LEN_FE, CAPACITY, DIMENSION>;
+    type PRF = ShakePRFtoF<HASH_LEN_FE, RAND_LEN_FE>;
+    type IE = TargetSumEncoding<MH, TARGET_SUM>;
+
+    pub type SIGTopLevelTargetSumLifetime32Dim64Base8 =
+        GeneralizedXMSSSignatureScheme<PRF, IE, TH, LOG_LIFETIME>;
+    pub type PubKeyTopLevelTargetSumLifetime32Dim64Base8 = GeneralizedXMSSPublicKey<TH>;
+    pub type SigTopLevelTargetSumLifetime32Dim64Base8 = GeneralizedXMSSSignature<IE, TH>;
+
+    #[cfg(test)]
+    mod test {
+
+        #[cfg(feature = "slow-tests")]
+        use super::*;
+        #[cfg(feature = "slow-tests")]
+        use crate::signature::SignatureScheme;
+
+        #[cfg(feature = "slow-tests")]
+        use crate::signature::test_templates::test_signature_scheme_correctness;
+
+        #[test]
+        #[cfg(feature = "slow-tests")]
+        pub fn test_correctness() {
+            test_signature_scheme_correctness::<SIGTopLevelTargetSumLifetime32Dim64Base8>(
+                213,
+                0,
+                SIGTopLevelTargetSumLifetime32Dim64Base8::LIFETIME as usize,
+            );
+            test_signature_scheme_correctness::<SIGTopLevelTargetSumLifetime32Dim64Base8>(
+                4,
+                0,
+                SIGTopLevelTargetSumLifetime32Dim64Base8::LIFETIME as usize,
+            );
+        }
+    }
+}
+
 /// Instantiations with Lifetime 2^6. This is for testing purposes only.
 ///
 /// Warning: Should not be used in production environments.
-///
-/// !!! TODO: compute properly each parameter. !!!
 pub mod lifetime_2_to_the_6 {
     use crate::{
         inc_encoding::target_sum::TargetSumEncoding,
@@ -17,7 +93,7 @@ pub mod lifetime_2_to_the_6 {
 
     // KoalaBear: p = 2^31 - 2^24 + 1 = 127 * 8^8 + 1
     // w=8, z=8, Q=127, alpha=1
-    const DIMENSION: usize = 64;
+    const DIMENSION: usize = 46;
     const BASE: usize = 8;
     const Z: usize = 8;
     const Q: usize = 127;
@@ -27,7 +103,7 @@ pub mod lifetime_2_to_the_6 {
     const TWEAK_LEN_FE: usize = 2;
     const MSG_LEN_FE: usize = 9;
     const RAND_LEN_FE: usize = 7;
-    const MH_HASH_LEN_FE: usize = 8;
+    const HASH_LEN_FE: usize = 8;
 
     const TH_HASH_LEN_FE: usize = 8;
     const CAPACITY: usize = 9;
@@ -35,7 +111,7 @@ pub mod lifetime_2_to_the_6 {
     type MH = AbortingHypercubeMessageHash<
         PARAMETER_LEN,
         RAND_LEN_FE,
-        MH_HASH_LEN_FE,
+        HASH_LEN_FE,
         DIMENSION,
         BASE,
         Z,
@@ -50,7 +126,7 @@ pub mod lifetime_2_to_the_6 {
     type PRF = ShakePRFtoF<TH_HASH_LEN_FE, RAND_LEN_FE>;
     type IE = TargetSumEncoding<MH, TARGET_SUM>;
 
-    pub type SIGAbortingLifetime6Dim64Base8 =
+    pub type SIGAbortingLifetime6Dim46Base8 =
         GeneralizedXMSSSignatureScheme<PRF, IE, TH, LOG_LIFETIME>;
 
     #[cfg(test)]
@@ -59,19 +135,19 @@ pub mod lifetime_2_to_the_6 {
             SignatureScheme, test_templates::test_signature_scheme_correctness,
         };
 
-        use super::SIGAbortingLifetime6Dim64Base8;
+        use super::SIGAbortingLifetime6Dim46Base8;
 
         #[test]
         pub fn test_correctness() {
-            test_signature_scheme_correctness::<SIGAbortingLifetime6Dim64Base8>(
+            test_signature_scheme_correctness::<SIGAbortingLifetime6Dim46Base8>(
                 2,
                 0,
-                SIGAbortingLifetime6Dim64Base8::LIFETIME as usize,
+                SIGAbortingLifetime6Dim46Base8::LIFETIME as usize,
             );
-            test_signature_scheme_correctness::<SIGAbortingLifetime6Dim64Base8>(
+            test_signature_scheme_correctness::<SIGAbortingLifetime6Dim46Base8>(
                 11,
                 0,
-                SIGAbortingLifetime6Dim64Base8::LIFETIME as usize,
+                SIGAbortingLifetime6Dim46Base8::LIFETIME as usize,
             );
         }
     }
