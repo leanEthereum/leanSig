9d37fa9a6aa25660b9f353972f7e6c9191aa65d3
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2026-04-04 11:07:04 +0200

    encoding: rm useless top of the hypercube encoding (#47)

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,14 +1,8 @@
 use criterion::{criterion_group, criterion_main};
 
 mod benchmark_poseidon;
-mod benchmark_poseidon_top_level;
 
 use benchmark_poseidon::bench_function_poseidon;
-use benchmark_poseidon_top_level::bench_function_poseidon_top_level;
 
-criterion_group!(
-    benches,
-    bench_function_poseidon_top_level,
-    bench_function_poseidon
-);
+criterion_group!(benches, bench_function_poseidon);
 criterion_main!(benches);
